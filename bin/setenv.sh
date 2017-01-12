1f159bf3cfa315335dde85c0fe02e175e6adff38
Igor Melnikov <turistpro@gmail.com>
2017-01-12 22:44:12 +0300

    increase PermSize to 256m

--- bin/setenv.sh
+++ bin/setenv.sh
@@ -1,1 +1,1 @@
-JAVA_OPTS="-Xms512m -Xmx1024m -XX:PermSize=64m -XX:MaxPermSize=384m -Djava.awt.headless=true"
+JAVA_OPTS="-Xms512m -Xmx1024m -XX:PermSize=256m -XX:MaxPermSize=384m -Djava.awt.headless=true"
